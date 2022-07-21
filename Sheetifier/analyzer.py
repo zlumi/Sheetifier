65033933f0f6e0391d282e022e763b65628abdfe
zClapr <48451623+zClapr@users.noreply.github.com>
2022-07-21 15:21:21 +0200

    reorganization & keypress check implemented
    also has customizability set up

--- Sheetifier/analyzer.py
+++ Sheetifier/analyzer.py
@@ -1,3 +1,5 @@
+import cv2
+
 def getKeyPositions(strtKey:str, nKeys:int, totalWidth:int, barYPos:int, blkwhtOffset:int) -> list:
     keys = []
     
@@ -47,3 +49,78 @@ def areColorsClose(value1, value2, tolerance:int):
     if abs(value1[0] - value2[0]) <= tolerance and abs(value1[1] - value2[1]) <= tolerance and abs(value1[2] - value2[2]) <= tolerance:
         return True
     return False
+
+def find_white_and_black_key_colors(vidPath, starting_key:str="F", total_keys:int=65):
+    vCap = cv2.VideoCapture(vidPath)
+    keys = getKeyPositions(
+        starting_key, total_keys, 
+        int(vCap.get(cv2.CAP_PROP_FRAME_WIDTH)), 
+        int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)) - int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)/5), 
+        50
+    )
+
+    detected_colors = {}
+
+    for frame in range(int(vCap.get(cv2.CAP_PROP_FRAME_COUNT))):
+        ret, frame = vCap.read()
+
+        for key in keys:
+            color_value = tuple(frame[
+                key[1][1], key[1][0]
+            ])
+
+            if str(color_value) not in detected_colors.keys():
+                detected_colors[str(color_value)] = 1
+            else:
+                detected_colors[str(color_value)] += 1
+
+    rankedPixelOccurancy = sorted(detected_colors, key=lambda k: detected_colors[k], reverse=True)
+    white_unpressed = rankedPixelOccurancy[0]
+
+    for color in rankedPixelOccurancy:
+        if not areColorsClose(white_unpressed, color, 50):
+            black_unpressed = color
+            break
+
+    return(white_unpressed, black_unpressed)
+
+def vid2dict(vidPath:str, starting_key:str="F", total_keys:int=65, white_unpressed:tuple=(239, 252, 254), black_unpressed:tuple=(20, 20, 20), closeness_tolerance:int=50) -> dict:
+    vCap = cv2.VideoCapture(vidPath)
+    keys = getKeyPositions(
+        starting_key, total_keys, 
+        int(vCap.get(cv2.CAP_PROP_FRAME_WIDTH)), 
+        int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)) - int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)/5), 
+        50
+    )
+
+    data = {}
+    for key in keys:
+        data[key[0]] = []
+    frame_number = 0
+
+    for frame in range(int(vCap.get(cv2.CAP_PROP_FRAME_COUNT))):
+        ret, frame = vCap.read()
+        frame_number += 1
+
+        for key in keys:
+            color_value = tuple(frame[
+                key[1][1], key[1][0]
+            ])
+
+            if "#" in key[0]:
+                if areColorsClose(color_value, black_unpressed, closeness_tolerance):
+                    data[key[0]].append("0")
+                else:
+                    data[key[0]].append("1")
+            else:
+                if areColorsClose(color_value, white_unpressed, closeness_tolerance):
+                    data[key[0]].append("0")
+                else:
+                    data[key[0]].append("1")
+
+    out = {}
+    for keyName in data.keys():
+        if "1" in data[keyName]:
+            out[keyName] = "".join(data[keyName])
+    
+    return out
--- Sheetifier/main.py
+++ Sheetifier/main.py
@@ -1,51 +1,8 @@
-import cv2
+from analyzer import vid2dict
+from numpy import uint8
 import json
-from analyzer import *
 
-piano_size = 65
-starting_key = "F"
-vCap = cv2.VideoCapture("./clips/low.mp4")
+data = vid2dict("clips/trimmed.mov")
 
-# Color values
-white = (239, 252, 254)
-black = (20, 20, 20)
-
-if __name__ == "__main__":
-    keys = getKeyPositions(
-        starting_key, piano_size, 
-        int(vCap.get(cv2.CAP_PROP_FRAME_WIDTH)), 
-        int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)) - int(vCap.get(cv2.CAP_PROP_FRAME_HEIGHT)/5), 
-        50
-    )
-
-    count = 0
-    data = {}
-    for key in keys:
-        data[key[0]] = []
-
-    # FIND KEY COLOURS
-
-    detected_colors = {}
-
-    for frame in range(int(vCap.get(cv2.CAP_PROP_FRAME_COUNT))):
-        ret, frame = vCap.read()
-
-        for key in keys:
-            color_value = tuple(frame[
-                key[1][1], key[1][0]
-            ])
-            data[key[0]].append(color_value)
-
-            if str(color_value) not in detected_colors.keys():
-                detected_colors[str(color_value)] = 1
-            else:
-                detected_colors[str(color_value)] += 1
-
-    rankedPixelOccurancy = sorted(detected_colors, key=lambda k: detected_colors[k], reverse=True)
-    white_unpressed = rankedPixelOccurancy[0]
-
-    for color in rankedPixelOccurancy:
-        if not areColorsClose(white_unpressed, color, 50):
-            black_unpressed = color
-            break
-    print(white_unpressed, black_unpressed)
+with open("data.json", "w") as f:
+    json.dump(data, f, default = lambda o: int(o) if isinstance(o,uint8) else o)
